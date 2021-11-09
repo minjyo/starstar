917beb4b7a1ee2db85578fb6ab2e1deb2d164bc8
minjyo <minjyo8823@gmail.com>
2021-11-09 20:59:57 +0900

    feat: add marker and popup

--- src/pages/MapPage.js
+++ src/pages/MapPage.js
@@ -1,16 +1,18 @@
-import React, { useState } from 'react';
-import ReactMapGL, { GeolocateControl } from 'react-map-gl';
+import React, { useState, useMemo, useEffect } from 'react';
+import ReactMapGL, { GeolocateControl, Marker, Popup } from 'react-map-gl';
 
 const MapPage = () => {
   const [lng, setLng] = useState(0);
   const [lat, setLat] = useState(0);
 
-  navigator.geolocation.getCurrentPosition(function (position) {
-    console.log('Latitude is :', position.coords.latitude);
-    console.log('Longitude is :', position.coords.longitude);
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(function (position) {
+      console.log('Latitude is :', position.coords.latitude);
+      console.log('Longitude is :', position.coords.longitude);
 
-    setLng(position.coords.longitude);
-    setLat(position.coords.latitude);
+      setLng(position.coords.longitude);
+      setLat(position.coords.latitude);
+    });
   });
 
   const MAPBOX_TOKEN =
@@ -27,6 +29,36 @@ const MapPage = () => {
     zoom: 14,
   });
 
+  // 임시 마커용 데이터 (다른 유저들 위치)
+  const data = [
+    { name: 'user1', longitude: lng - 0.001, latitude: lat - 0.001 },
+    { name: 'user2', longitude: lng + 0.001, latitude: lat + 0.001 },
+  ];
+
+  const [showPopup, togglePopup] = useState(false);
+  // Only rerender markers if props.data has changed
+  const markers = useMemo(
+    () =>
+      data.map((user) => (
+        <Marker
+          key={user.name}
+          longitude={user.longitude}
+          latitude={user.latitude}
+          onClick={() => togglePopup(true)}
+        >
+          <div
+            style={{
+              width: 10,
+              height: 10,
+              borderRadius: 10,
+              background: 'white',
+            }}
+          ></div>
+        </Marker>
+      )),
+    [data],
+  );
+
   return (
     <ReactMapGL
       {...viewport}
@@ -36,6 +68,19 @@ const MapPage = () => {
       onViewportChange={setViewport}
       mapboxApiAccessToken={MAPBOX_TOKEN}
     >
+      {markers}
+      {showPopup && (
+        <Popup
+          tipSize={5}
+          anchor="top"
+          longitude={data[0].longitude + 0.0001}
+          latitude={data[0].latitude - 0.0005}
+          closeOnClick={false}
+          onClose={() => togglePopup(false)}
+        >
+          응애
+        </Popup>
+      )}
       <GeolocateControl
         style={geolocateControlStyle}
         positionOptions={{ enableHighAccuracy: true }}
